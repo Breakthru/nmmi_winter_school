abd6957f5b46a16cc552934632030f9c7dbdc359
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-02-20 17:50:16 +0100

    Added empty skeleton of new driver node.

---
+++ iai_qb_cube_driver/include/iai_qb_cube_driver/Driver.hpp
@@ -0,0 +1,17 @@
+#ifndef IAI_QB_CUBE_DRIVER_DRIVER_HPP
+#define IAI_QB_CUBE_DRIVER_DRIVER_HPP
+
+#include <qbmove_communications.h>
+
+namespace iai_qb_cube_driver
+{
+  class Driver
+  {
+    public:
+      Driver() {}
+  
+      ~Driver() {}
+  };
+}
+
+#endif // IAI_QB_CUBE_DRIVER_DRIVER_HPP
---
+++ iai_qb_cube_driver/src/iai_qb_cube_driver/main.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include "iai_qb_cube_driver/Driver.hpp"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "iai_qb_cube_driver");
+  ros::NodeHandle n("~");
+  iai_qb_cube_driver::Driver my_driver();
+  return 0;
+}

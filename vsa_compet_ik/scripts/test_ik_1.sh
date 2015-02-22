95f6bb1ae2cf34e877d5da9ecd35f3438abc5ac3
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2015-02-22 22:51:14 +0000

    first script for testing the ik

---
+++ vsa_compet_ik/scripts/test_ik_1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rostopic pub -1 /arm_controller/command geometry_msgs/TransformStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'left_holder_link'
+child_frame_id: 'arm_fixed_finger'
+transform:
+  translation:
+    x: 0.0
+    y: $1
+    z: 0.0
+  rotation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0" 
+
+
